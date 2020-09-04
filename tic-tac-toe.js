c115c945be03c712ac9ca8086346863cfd68a87d
salina kuo <salina.kuo97@gmail.com>
2020-09-04 13:52:36 -0400

    Click images

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,20 +1,32 @@
-let currentPlayerSymbol = "X";
+let currentPlayerSymbol = "x";
 let squareValues = ["", "", "", "", "", "", "", "", ""];
+let gameStatus = '';
+
+const checkGameStatus = function() {
+    for (let i = 0; i < squareValues.length; i+2) {
+        let row = currentPlayerSymbol
+    }
+}
 
 window.addEventListener("DOMContentLoaded", event => {
     const grid = document.getElementById('tic-tac-toe-board')
     grid.addEventListener('click', event => {
+        let image = document.createElement('img');
+        let square = document.getElementById(event.target.id)
+        image.setAttribute('src', `images/player-${currentPlayerSymbol}.svg`);
+        square.appendChild(image);
         if (event.target.id.includes('square-')) {
             let num = Number.parseInt(event.target.id[7])
             if (squareValues[num] === "") {
                 squareValues[num] = currentPlayerSymbol;
             } else {
-                return
             }
-            console.log(squareValues);
         };
-        let image = document.createElement('img');
-        let x = image.setAttribute('src', '/images/player-x.svg');
-        let o = image.setAttribute('src', '/images/player-o.svg');
+        if (currentPlayerSymbol === 'x') {
+            currentPlayerSymbol = 'o';
+
+        } else {
+            currentPlayerSymbol = 'x';
+        }
     });
 });
